36781f8bd1d7d6ecdee21f12587b3d1fc0d98fcc
Sachiro <sachiro.20232147@iit.ac.lk>
2026-04-20 08:36:39 +0530

    Added logging filters

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.smartcampus.filter;
+
+import jakarta.ws.rs.container.*;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger logger = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        logger.info("Request: " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        logger.info("Response status: " + responseContext.getStatus());
+    }
+}
